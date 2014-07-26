75ee91cfefef0b2f4c7888cf804afd4085a1925c
Violet-Knight <CptnJustc@Mega-Book-5.local>
2014-07-26 18:46:37 -0400

    Matrix conversion
    Implementation of matrix inversion caching object

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating an object containing a matrix and its inverse,
+## solving for the inverse, and caching the inverse to ease computation
 
-## Write a short comment describing this function
+## Creates an object that stores a matrix and its inverse, with methods
+## for getting and setting both
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Takes object created by makeCacheMatrix, solves for and caches its
+## inverse, or returns the cached inverse if available
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
